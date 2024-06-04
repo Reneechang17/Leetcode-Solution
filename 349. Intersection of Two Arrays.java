1fc20b5433adae7dbb97c4a3f640897dd822de22
Reneechang17 <136089488+Reneechang17@users.noreply.github.com>
2024-06-03 23:30:49 -0400

    6/3

---
+++ 349. Intersection of Two Arrays.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Easy
+// Hash Table, Two Pointers, Array
+// O(n)
+
+class Solution {
+  public int[] intersection(int[] nums1, int[] nums2) {
+      int[] hash1 = new int[1001];
+      int[] hash2 = new int[1001];
+      for(int i : nums1){
+          hash1[i]++;
+      }
+      for(int i : nums2){
+          hash2[i]++;
+      }
+      List<Integer> res = new ArrayList<>();
+      for(int i = 0; i < 1001; i++){
+          if(hash1[i] > 0 && hash2[i] > 0){
+              res.add(i);
+          }
+      }
+      int idx = 0;
+      int[] resList = new int[res.size()];
+      for(int i : res){
+          resList[idx] = i;
+          idx++;
+      }
+      return resList;
+  }
+}
