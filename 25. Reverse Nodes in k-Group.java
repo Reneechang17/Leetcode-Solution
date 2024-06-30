ad00df9e035be1f2eceac75076fb9470b1439be9
Reneechang17 <136089488+Reneechang17@users.noreply.github.com>
2024-06-30 15:43:58 -0400

    6/30

--- 25. Reverse Nodes in k-Group.java
+++ 25. Reverse Nodes in k-Group.java
@@ -4,23 +4,6 @@
 // https://leetcode.com/problems/reverse-nodes-in-k-group/
 
 class Solution {
-    public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
     public ListNode reverseKGroup(ListNode head, int k) {
         if (head == null)
             return null;
