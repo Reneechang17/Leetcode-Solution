83c823b81453d90396855d7b8203354f8c4ef980
Reneechang17 <minnie90061717@gmail.com>
2024-08-12 00:04:30 -0400

    8/11

--- 215. Kth Largest Element in an Array.java
+++ 215. Kth Largest Element in an Array.java
@@ -4,55 +4,58 @@
 // https://leetcode.com/problems/kth-largest-element-in-an-array/
 
 class Solution {
-  public int findKthLargest(int[] nums, int k) {
-      int n = nums.length;
-      // 把問題轉換成找第nums.length - k小的元素
-      return quickSelect(nums, 0, n - 1, n - k);
-  }
-  private int quickSelect (int[] nums, int left, int right, int k) {
-      // 當搜索範圍縮小到只有一個元素的時候，直接返回那個元素
-      if (left == right) { 
-          return nums[left];
-      }
-      // 計算中點
-      int splitIndex = (left + right) >>> 1;
-      int split = nums[splitIndex];
-      int i = left, j = right;
+    public int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+        // 把問題轉換成找第nums.length - k小的元素
+        return quickSelect(nums, 0, n - 1, n - k);
+    }
 
-      while (i <= j) {
-          while (nums[i] < split) {
-              i++;
-          }
-          while (nums[j] > split) { 
-              j--;
-          }
-          if (i <= j) {
-              swap (nums, i, j);
-              i++;
-              j--;
-          }
-      }
-      if (k <= j) {
-          return quickSelect (nums, left, j, k);
-      }
-      if (k >= i) {
-          return quickSelect (nums, i, right, k);
-      }
-      return nums[k];
-  }
+    private int quickSelect(int[] nums, int left, int right, int k) {
+        // 當搜索範圍縮小到只有一個元素的時候，直接返回那個元素
+        if (left == right) {
+            return nums[left];
+        }
+        // 計算中點
+        int splitIndex = (left + right) >>> 1;
+        int split = nums[splitIndex];
+        int i = left, j = right;
 
-  private void swap (int[] nums, int a, int b) {
-      int temp = nums[a];
-      nums[a] = nums[b];
-      nums[b] = temp;
-  }
+        while (i <= j) {
+            while (nums[i] < split) {
+                i++;
+            }
+            while (nums[j] > split) {
+                j--;
+            }
+            if (i <= j) {
+                swap(nums, i, j);
+                i++;
+                j--;
+            }
+        }
+        if (k <= j) {
+            return quickSelect(nums, left, j, k);
+        }
+        if (k >= i) {
+            return quickSelect(nums, i, right, k);
+        }
+        return nums[k];
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
 }
 
 /**
+ * 數組中第k大的元素
  * 首先這題讓我們避免使用sorting所以就不要往sorting去思考
  * 
  * 這題可以用快速選擇(quickSelect)方法 => 用於查找數組中第k個最大（最小）元素很有效，可以將時間複雜度壓到O(n)
- * review：快速選擇算法操作 1. 選擇一個樞紐元素， 通常是數組的第一個元素（left） 2.分區（把比樞紐元素小的都放到左邊，反之放右邊）3. 遞歸：樞紐元素的位置可以用來判斷第k個最大or最小元素在左邊還是右邊，如果樞紐元素正好是k，就返回k；如果樞紐元素比k小，就在數組的右邊找；如果樞紐元素比k大，就在數組的左邊找
+ * review：快速選擇算法操作 1. 選擇一個樞紐元素， 通常是數組的第一個元素（left） 2.分區（把比樞紐元素小的都放到左邊，反之放右邊）3.
+ * 遞歸：樞紐元素的位置可以用來判斷第k個最大or最小元素在左邊還是右邊，如果樞紐元素正好是k，就返回k；如果樞紐元素比k小，就在數組的右邊找；如果樞紐元素比k大，就在數組的左邊找
  * 
  * 這題是找第k個最大元素，那反過來就是第nums.length - k 個最小元素
  * 
---
+++ 703. Kth Largest Element in a Stream.java
@@ -0,0 +1,40 @@
+// Easy
+// PriorityQueue, Heap
+// O(log k)
+// https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+import java.util.PriorityQueue;
+
+class KthLargest {
+  private PriorityQueue<Integer> minHeap;
+  private int k;
+
+  public KthLargest(int k, int[] nums) {
+      this.k = k;
+      minHeap = new PriorityQueue<>(k);
+      for (int num : nums) {
+          add(num);
+      }
+  }
+  
+  public int add(int val) {
+      if (minHeap.size() < k) {
+          minHeap.offer(val);
+      } else if (val > minHeap.peek()) {
+          minHeap.poll();
+          minHeap.offer(val);
+      }
+      return minHeap.peek();
+  }
+}
+
+/**
+ * 要求實現一個數據結構，能夠持續返回數據流中第k大的元素
+ * 注意他是排序後第k大的元素，不是第k個不同的元素
+ * 
+ * 這種找第k大的元素，可以聯想到最小堆的做法（優先隊列）
+ * 透過維護一個大小為k的最小堆，其堆頂的元素就是第k大的元素
+ * 如果堆的大小小於k時，直接將新元素加入堆中
+ * 如果堆已經滿了，並且新元素大於堆頂元素，則彈出堆頂元素，將新元素加入堆中
+ * 如此一來，堆頂的元素始終是第k大的元素
+ **/
