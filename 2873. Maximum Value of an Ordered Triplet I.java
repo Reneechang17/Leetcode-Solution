0b3cecd244fd45a7d2e68f7dfc5f5fabb82cd24c
Reneechang17 <minnie90061717@gmail.com>
2025-04-03 00:03:41 -0400

    4/3

--- 2873. Maximum Value of an Ordered Triplet I.java
+++ 2873. Maximum Value of an Ordered Triplet I.java
@@ -1,17 +1,17 @@
 // Easy
 // Brute Force
-// Time:O(n^3),Space:O(1)
+// Time:O(n^2),Space:O(1)
 // https://leetcode.cn/problems/maximum-value-of-an-ordered-triplet-i/
 
 class Solution {
     public long maximumTripletValue(int[] nums) {
         int n = nums.length;
         long res = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j; k < n; k++) {
-                    res = Math.max(res, (long)(nums[i] - nums[j]) * nums[k]);
-                }
+        for (int k = 2; k < n; k++) {
+            int m = nums[0];
+            for (int j = 1; j < k; j++) {
+                res = Math.max(res, (long)(m - nums[j]) * nums[k]);
+                m = Math.max(m, nums[j]);
             }
         }
         return res;
---
+++ 2874. Maximum Value of an Ordered Triplet II.java
@@ -0,0 +1,20 @@
+// Medium
+// Greedy
+// Time:O(n), Space:O(n)
+// https://leetcode.cn/problems/maximum-value-of-an-ordered-triplet-ii/
+
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        int n = nums.length;
+        int[] leftMax = new int[n], rightMax = new int[n];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], nums[i - 1]);
+            rightMax[n - 1 - i] = Math.max(rightMax[n - i], nums[n - i]);
+        }
+        long res = 0;
+        for (int j = 1; j < n - 1; j++) {
+            res = Math.max(res, (long)(leftMax[j] - nums[j]) * rightMax[j]);
+        }
+        return res;
+    }
+}
