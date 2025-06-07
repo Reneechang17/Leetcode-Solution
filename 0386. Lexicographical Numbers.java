9b34e28c131bd4720f055166de9e4c833adb0912
Reneechang17 <minnie90061717@gmail.com>
2025-06-07 13:32:25 -0700

    6/7

---
+++ 0386. Lexicographical Numbers.java
@@ -0,0 +1,25 @@
+// Medium
+// Math
+// Time:O(n),Space:O(1)
+// https://leetcode.cn/problems/lexicographical-numbers/
+
+import java.util.*;
+
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> res = new ArrayList<>();
+        int num = 1;
+        for (int i = 0; i < n; i++) {
+            res.add(num);
+            if (num * 10 <= n) {
+                num *= 10;
+            } else {
+                while (num % 10 == 9 || num + 1 > n) {
+                    num /= 10;
+                }
+                num++;
+            }
+        }
+        return res;
+    }
+}
